1cabbe67e53bf9e76741c3df8b0f45d65c5ecb37
vipuL2701 <vikratsethji@gmail.com>
2024-01-01 19:15:43 +0530

    responsive

--- TANDOORI/src/App.jsx
+++ TANDOORI/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import AboutUs from './Components/About/AboutUs'
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+       <Header />
+        <Routes>
+             <Route path='/' exact Component={Home} />
+             <Route path='about' Component={AboutUs} />
+            
+         </Routes>
+       <Home />
+    </Router>
   )
 }
 
